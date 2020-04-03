3446e5273bc6392eaeca5c1f2376527beccbe0b9
nazareh <nazarehturmina@gmail.com>
2020-04-03 17:47:39 +0800

    Add Rank component and particles.js background

--- src/App.js
+++ src/App.js
@@ -2,16 +2,33 @@ import React from 'react';
 import './App.css';
 import Navigation from "./components/navigation/Navigation";
 import Logo from "./components/logo/Logo";
-import ImageLinkForm from "./components/imageLinkForm/ImageLinkForm"
+import ImageLinkForm from "./components/imageLinkForm/ImageLinkForm";
+import Rank from './components/rank/Rank';
+import Particles from 'react-particles-js';
+
+const particlesOptions =
+    {
+        particles: {
+            number: {
+                value: 80,
+                density: {
+                    enable: true,
+                    value_area: 800
+                }
+            }
+        },
+    };
 
 function App() {
-  return (
-    <div className="App">
-      <Navigation/>
-      <Logo/>
-      <ImageLinkForm/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Particles className='particles' params={particlesOptions}/>
+            <Navigation/>
+            <Logo/>
+            <Rank/>
+            <ImageLinkForm/>
+        </div>
+    );
 }
 
 export default App;
