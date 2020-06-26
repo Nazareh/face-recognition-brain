c01e91bf7e6a0efe22f85ee2b427b2305b8bad8c
Nazare <nazarehturmina@gmail.com>
2020-06-26 09:21:01 +0800

    Add initial state to a variable

--- src/App.js
+++ src/App.js
@@ -27,23 +27,25 @@ const particlesOptions =
         },
     };
 
+const initialState = {
+    input: '',
+    imageUrl: '',
+    box: {},
+    route: 'signin',
+    isSignedIn: false,
+    user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+    }
+}
+
 class App extends React.Component {
     constructor() {
         super();
-        this.state = {
-            input: '',
-            imageUrl: '',
-            box: {},
-            route: 'signin',
-            isSignedIn: false,
-            user: {
-                id: '',
-                name: '',
-                email: '',
-                entries: 0,
-                joined: ''
-            }
-        }
+        this.state = initialState;
     }
 
     loadUser = (data) => {
@@ -96,6 +98,7 @@ class App extends React.Component {
                     })
                         .then(res => res.json())
                         .then(count => this.setState(Object.assign(this.state.user, {entries: count})))
+                        .catch(console.log)
                 }
                 this.displayFaceBox(this.calculateFaceLocation(response))
             })
@@ -104,7 +107,7 @@ class App extends React.Component {
 
     onRouteChange = (route) => {
         if (route === 'signout') {
-            this.setState({isSignedIn: false})
+            this.setState(initialState)
         } else if (route === 'home') {
             this.setState({isSignedIn: true})
         }
