354a5fffaa795dc5abb39b2f384f1408c28a3d8f
Nazare <nazarehturmina@gmail.com>
2020-06-26 18:46:57 +0800

    Add Clarifi to the back end instead of front end

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import './App.css';
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
@@ -9,10 +8,6 @@ import Rank from './components/Rank/Rank';
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Signin from "./components/Signin/Signin";
 import Register from "./components/Register/Register";
-
-const app = new Clarifai.App({
-    apiKey: 'ef1582b0063d4b87a1e2e624f79c61e9'
-});
 
 const particlesOptions =
     {
@@ -84,9 +79,14 @@ class App extends React.Component {
 
     onPictureSubmit = () => {
         this.setState({imageUrl: this.state.input})
-        app.models.predict(
-            Clarifai.FACE_DETECT_MODEL,
-            this.state.input)
+        fetch('http://localhost:3002/imageUrl', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                input: this.state.input
+            })
+        })
+            .then(response => response.json())
             .then(response => {
                 if (response) {
                     fetch('http://localhost:3002/image', {
